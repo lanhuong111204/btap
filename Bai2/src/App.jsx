4fffa5aa819aad02022827f02d7997a8e3662cb1
lanhuong111204 <doanlanhuong11a1nhnd@gmail.com>
2025-03-03 23:17:32 +0700

    Initial commit

---
+++ Bai2/src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MyComponent from "./components/MyComponent";
+
+function App() {
+
+  return (
+      <div>
+       <MyComponent></MyComponent>  
+      </div>
+  )
+}
+
+export default App
