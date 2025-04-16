f76ce0c8e51befadc9582ac4151fe0ec74b73bfb
lanhuong111204 <doanlanhuong11a1nhnd@gmail.com>
2025-04-16 12:45:34 +0700

    2.API overview

--- tuan7/src/components/Overview.jsx
+++ tuan7/src/components/Overview.jsx
@@ -1,13 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import logo_cart from"../assets/img/logo_cart.png";
+import logo_$ from"../assets/img/logo_$.png";
+import logo_user from"../assets/img/logo_user.png";
 
 const Overview =() => {
   const [overviewData, setOverviewData] = useState([]);
+
+  const imgMap = {
+    logo_cart: <img src={logo_cart} alt="" />,
+    logo_$: <img src={logo_$} alt="" />,
+    logo_user: <img src={logo_user} alt="" />
+  }
 
   useEffect(() => {
     fetch("http://localhost:3001/overview")
       .then((response) => response.json())
       .then((data) => setOverviewData(data))
   }, []);
+
+  console.log("overviewdata: ", overviewData);
+  
 
   return (
     <div>
@@ -27,7 +39,7 @@ const Overview =() => {
               <p className="text-xl font-bold">{item.data}</p>
               <p className="text-green-500 text-sm">{item.period}</p>
             </div>
-            <img src={item.img}/>
+            {imgMap[item?.img]}
           </div>
         ))}
       </div>
--- tuan7/src/components/Table.jsx
+++ tuan7/src/components/Table.jsx
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import {EditModal} from './EditModal';
 import {AddModal} from './AddModal';
+import Avatar from "../assets/img/Avatar.png"
+import Avatar1 from "../assets/img/Avatar (1).png"
+import Avatar2 from "../assets/img/Avatar (2).png"
+import Avatar3 from "../assets/img/Avatar (3).png"
+import Avatar4 from "../assets/img/Avatar (4).png"
+import Avatar5 from "../assets/img/Avatar (5).png"
+import Edit from "../assets/img/create.png"
+import File from "../assets/img/icon_textfield.png"
+import Download from "../assets/img/Download.png"
+import MoveUp from"../assets/img/Move up.png"
 
 const statusColor = {
   'New': 'text-blue-500 bg-blue-100',
@@ -20,6 +30,15 @@ const Table = () => {
       .then((data) => setInfoData(data))
       .catch((error) => console.error("Lỗi khi tải dữ liệu:", error));
   }, []);
+
+  const avatarMap = {
+    avatar: <img src={Avatar} alt="" className="w-8 h-8 rounded-full" />,
+    avatar1: <img src={Avatar1} alt="" className="w-8 h-8 rounded-full" />,
+    avatar2: <img src={Avatar2} alt="" className="w-8 h-8 rounded-full" />,
+    avatar3: <img src={Avatar3} alt="" className="w-8 h-8 rounded-full" />,
+    avatar4: <img src={Avatar4} alt="" className="w-8 h-8 rounded-full" />,
+    avatar5: <img src={Avatar5} alt="" className="w-8 h-8 rounded-full" />,
+  }
 
   const handleEditClick = (user) => {
     setSelectedUser(user);
@@ -56,15 +75,15 @@ const Table = () => {
   return (
     <div>
       <div className="flex items-center gap-2 mb-4 p-3">
-        <img src="File text 1.png" alt="overview icon" className="w-5 h-5" />
+        <img src={File} alt="overview icon" className="w-5 h-5" />
         <h5 className="text-lg font-semibold">Detailed Report</h5>
         <div className="flex gap-2">
           <button className="flex items-center gap-1 px-3 py-1 text-pink-600 border border-pink-500 rounded-lg">
-            <img src="Download.png" alt="Import icon" />
+            <img src={Download} alt="Import icon" />
             <span>Import</span>
           </button>
           <button className="flex items-center gap-1 px-3 py-1 text-pink-600 border border-pink-500 rounded-lg">
-            <img src="Move up.png" alt="Export icon" />
+            <img src={MoveUp} alt="Export icon" />
             <span>Export</span>
           </button>
         </div>
@@ -92,7 +111,7 @@ const Table = () => {
                   <input type="checkbox" />
                 </td>
                 <td className="flex items-center gap-2 py-2">
-                  <img src={row.avatar} alt="" className="w-8 h-8 rounded-full" />
+                  {avatarMap[row.avatar]}
                   <span className='font-bold'>{row.name}</span>
                 </td>
                 <td>{row.company}</td>
@@ -105,7 +124,7 @@ const Table = () => {
                 </td>
                 <td>
                   <button onClick={() => handleEditClick(row)}>
-                    <img src="create.png" alt="Edit icon" />
+                    <img src={Edit} alt="Edit icon" />
                   </button>
                 </td>
               </tr>
